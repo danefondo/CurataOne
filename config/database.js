ed1b7513ace27668e4121c45318991721a665169
Robert Unyx <robertunyx@gmail.com>
2019-12-07 16:10:16 +0200

    yep

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 module.exports = {
   db_uri: process.env.DB_URI,
   secret: process.env.SECRET,
